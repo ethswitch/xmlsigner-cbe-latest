a0a4aff3180cc40c9c3feb74cd6198e76817662d
tttesfaye <teklutesfayeh@gmail.com>
2025-05-08 17:47:58 +0300

    upgrade

--- src/main/java/org/ips/xml/signer/xmlsigner/service/PodNameMDCInitializer.java
+++ src/main/java/org/ips/xml/signer/xmlsigner/service/PodNameMDCInitializer.java
@@ -1,18 +1,21 @@
 package org.ips.xml.signer.xmlsigner.service;
 
 import javax.annotation.PostConstruct;
+
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class PodNameMDCInitializer {
-
-    @Value("${POD_NAME:unknown}")
+    @Value("${POD_NAME:unknown}") // or ${HOSTNAME}
     private String podName;
 
     @PostConstruct
     public void init() {
-        MDC.put("POD_NAME", podName);
+        MDC.put("POD_NAME", podName); // Add to MDC
+        log.info("Pod name initialized: {}", podName);
     }
 }
